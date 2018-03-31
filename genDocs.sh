4c5cfe6b06f70f7ba9c914744591b7debd3b3c8d
Alejandro Rodríguez <id00681749@usal.es>
2018-03-31 16:35:11 +0200

    documentation

---
+++ genDocs.sh
@@ -0,0 +1,26 @@
+mv ./dist ./_dist
+mv ./node_modules ./_node_modules
+echo '
+{
+    "plugins": [],
+    "recurseDepth": 10,
+    "source": {
+        "includePattern": ".+\\.(t|j)s(doc|x)?$",
+        "excludePattern": "(^|\\/|\\\\)_"
+    },
+    "sourceType": "module",
+    "tags": {
+        "allowUnknownTags": true,
+        "dictionaries": ["jsdoc","closure"]
+    },
+    "templates": {
+        "cleverLinks": false,
+        "monospaceLinks": false
+    }
+}
+
+' > conf.json
+jsdoc -c conf.json  --destination ./doc  -r .
+rm conf.json
+mv ./_dist ./dist
+mv ./_node_modules ./node_modules
